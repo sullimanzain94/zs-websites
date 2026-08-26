87badb80b656de013526cbaadf954d8e200887db
sullimanzain94 <sullimanzain94@gmail.com>
2026-08-26 13:15:39 +0200

    fix dependencies

---
+++ extract-deps.js
@@ -0,0 +1,11 @@
+﻿const lock = require('./package-lock.json');
+console.log('lockfileVersion:', lock.lockfileVersion);
+console.log('name:', lock.name);
+console.log('version:', lock.version);
+console.log('top-level keys:', Object.keys(lock));
+if (lock.dependencies) {
+  console.log('dependency names:', Object.keys(lock.dependencies));
+}
+if (lock.packages) {
+  console.log('packages[\"\"]:', JSON.stringify(lock.packages[''], null, 2));
+}
